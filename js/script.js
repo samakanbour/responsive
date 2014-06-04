8e46d7a494e305ef3b199964dba453bb40f6f070
Kanbour Sama <samak@cmu.edu>
2014-06-04 21:25:49 +0300

    files added

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$("#js-mobile-navigation-button").click(function () {
+		return $("#js-mobile-navigation td").each(function (e) {
+			var t = this;
+			return setTimeout(function () {
+				return $(t).addClass("active")
+			}, e * 50)
+		})
+	}), $("td a").click(function () {
+		return $("#js-mobile-navigation td").each(function (e) {
+			var t = this;
+			return setTimeout(function () {
+				return $(t).removeClass("active")
+			}, e * 50)
+		})
+	}),	$("aside .nav li").hover(function (){
+		var t = $(this).children()[1];
+		var length = 0;
+		$(t).children().each(function (i, e){
+			length += $(e).height();
+		});
+		$(t).css("transition", ".5s ease");
+		$(t).css("height", length + $(t).children().length * 20);
+	}, function(){
+		var t = $(this).children()[1];
+		$(t).css("transition", "0.2s ease");
+		$(t).css("height", 0);
+	});
+});
